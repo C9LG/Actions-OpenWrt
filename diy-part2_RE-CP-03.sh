cb80a80306b75def1e275050d0ee81fb6a46489c
Sawyer <928331892@qq.com>
2025-05-19 01:00:49 +0800

    Update diy-part2_RE-CP-03.sh

--- diy-part2_RE-CP-03.sh
+++ diy-part2_RE-CP-03.sh
@@ -24,4 +24,4 @@ sed -i "s/ImmortalWrt/RE-CP-03/g" package/mtk/applications/mtwifi-cfg/files/mtwi
 sed -i "s/channel=\"[0-9]*\"/channel=\"auto\"/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i "s/encryption=.*/encryption='sae-mixed'/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='123456789'" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i "/set wireless\.\${dev}\.country=CN/set wireless.\${dev}.country=AU/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/set wireless\.\${dev}\.country=CN/set wireless.\${dev}.country=AU/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
