7cc1ffba90011186829e273a1b26c5f1b4607bdd
Sawyer <928331892@qq.com>
2025-05-18 17:59:58 +0800

    Update diy-part2_RE-CP-03.sh

--- diy-part2_RE-CP-03.sh
+++ diy-part2_RE-CP-03.sh
@@ -11,10 +11,17 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.6.1/192.168.2.4/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.2.4/g" /package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/JDCloud/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='JDCloud'/g" package/base-files/files/bin/config_generate
+
+# Modify WiFi
+sed -i "s/ImmortalWrt/RE-CP-03/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/channel=\"[0-9]*\"/channel=\"auto\"/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/encryption=.*/encryption='sae-mixed'/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='123456789'" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "/set wireless\.\${dev}\.country=CN/set wireless.\${dev}.country=AU/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
