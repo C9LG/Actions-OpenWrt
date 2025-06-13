8b7e614cad70df0dfc135e28631f35ad4342025d
Sawyer <928331892@qq.com>
2025-06-14 02:26:27 +0800

    Update diy-part2_Newifi-D2.sh

--- diy-part2_Newifi-D2.sh
+++ diy-part2_Newifi-D2.sh
@@ -11,20 +11,17 @@
 #
 
 # Modify default IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.2.5/g" /package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.2.5/g" package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
 
 # Modify hostname
 sed -i "s/hostname='.*'/hostname='Newifi-D2'/g" package/base-files/files/bin/config_generate
 
 # Modify WiFi
-sed -i "s/LEDE/Newifi-D2/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/ssid='.*'/ssid='Newifi-D2'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # sed -i "s/channel=\"[0-9]*\"/channel=\"auto\"/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i "s/encryption=.*/encryption='sae-mixed'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i "/set wireless.default_radio\${devidx}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_radio\${devidx}.key='123456789'" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i "s/set wireless.radio\${devidx}.country=US/set wireless.\${devidx}.country=AU/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/encryption='.*'/encryption='sae-mixed'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/key='.*'/key='123456789'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/country='.*'/country='AU'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
